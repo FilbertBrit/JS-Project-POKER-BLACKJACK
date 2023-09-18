6950d0fb38d810d1b457d8ad575d8a9f7276ad6b
Brittiny Filbert <britfilbert@icloud.com>
2023-09-17 18:43:25 -0700

    Working on logic for bestHand function

--- src/index.js
+++ src/index.js
@@ -4,28 +4,29 @@
 import { deckCreation } from "./scripts/cards.js"; // importing function that created the deck
 import * as UTIL from "./scripts/util.js" // importing arrays of card values
 
-const canvas = document.getElementById("canvas")
-canvas.width = 500;
-canvas.height = 500;
+// const canvas = document.getElementById("canvas")
+// canvas.width = 500;
+// canvas.height = 500;
 
-class multiChoice {
+class multiChoice { 
     constructor(){
         // this.ctx = canvas.getContext('2d');
+
         this.deck = deckCreation(); // deck used for multi-choice game
         this.players = []; //array that will hold all hands of the current game
         this.dealer = new PlayerHand(this.deck);
-        this.dealer.addToHand.call(this.dealer, 3)
+        this.dealer.addToHand.call(this.dealer, 3) // dealer always starts off with 3 cards
     }
 
-    hands(num){ // function for generating the num of hands wanted per game
+    hands(num){ // function for generating the num of players/hands wanted per game
         for(let i = 0; i < num; i++){
-            let player = new PlayerHand(this.deck) // creating a hand that also has a reference to the games deck
-            player.addToHand.call(player, 2) //hands will only ever have two cards
-            this.players.push(player) //adding each hand to the players array of the game
+            let player = new PlayerHand(this.deck) // creating a hand that also has a reference to the game's deck
+            player.addToHand.call(player, 2) //players/hands will only ever have two cards
+            this.players.push(player) //adding each hand to game's players array
         }
     }
 
-    printGame(){
+    printGame(){ // prints all cards in the game(dealer + players)
         console.log(this.dealer.hand)
         for(let i = 0; i < this.players.length; i++){
             console.log(this.players[i].hand);
@@ -33,30 +34,33 @@ class multiChoice {
     }
 
     //not too sure if we need this yet
-    handValues(){
-        for(let i = 0; i < this.players.length; i++){
-           let hand = this.players[i].hand
-           let value = 0
-           for(let i = 0; i < hand.length; i++){
-            // console.log(hand[i])
-                if(typeof(hand[i].value) === "number"){
-                    // console.log(hand[0].value)
-                    value += hand[i].value;
-                }else{
-                    value += UTIL.letterValue[hand[i].value]
-                }
-            }
-            hand.push(value);
-        }
-    }
+    // handValues(){
+    //     for(let i = 0; i < this.players.length; i++){
+    //        let hand = this.players[i].hand
+    //        let value = 0
+    //        for(let i = 0; i < hand.length; i++){
+    //         // console.log(hand[i])
+    //             if(typeof(hand[i].value) === "number"){
+    //                 // console.log(hand[0].value)
+    //                 value += hand[i].value;
+    //             }else{
+    //                 value += UTIL.letterValue[hand[i].value]
+    //             }
+    //         }
+    //         hand.push(value);
+    //     }
+    // }
 
     bestHand(round){
-        if(round === 1){ //first round only check players cards not dealers
-            let tie = false;
+        let tie = false;
+        let hand;
+        let currentHand;
+
+        if(round < 3){ //first round only check players cards not dealers
             // debugger
-            let hand = this.players[0].hand;
+            hand = this.players[0].hand;
             for(let i = 1; i < this.players.length; i++){
-                let currentHand = this.players[i].hand;
+                currentHand = this.players[i].hand;
                 //booleans checking if any of the two have a pair
                 let handPair = hand[0].value === hand[1].value;
                 let currentHandPair = currentHand[0].value === currentHand[1].value; 
@@ -66,7 +70,7 @@ class multiChoice {
                         if(hand[0].value === currentHand[0].value){
                             tie = true;
                         }else{
-                            hand = (hand[0].value > currentHand[0].value ? hand : currentHand)
+                            hand = (UTIL.values.indexOf(hand[0].value) > UTIL.values.indexOf(currentHand[0].value) ? hand : currentHand)
                         }
                     }else{
                         hand = (handPair ? hand : currentHand)
@@ -74,27 +78,83 @@ class multiChoice {
 
 
                 }else{ // no pair, just checking for highest card
-                    let handHighest = (hand[0].value > hand[1].value ? hand[0].value : hand[1].value)
-                    let currentHandHighest = (currentHand[0].value > currentHand[1].value ? currentHand[0].value : currentHand[1].value)
+                    let handHighest = (UTIL.values.indexOf(hand[0].value) > UTIL.values.indexOf(hand[1].value) ? hand[0].value : hand[1].value);
+                    let currentHandHighest = (UTIL.values.indexOf(currentHand[0].value) > UTIL.values.indexOf(currentHand[1].value) ? currentHand[0].value : currentHand[1].value)
 
-                    hand = (handHighest > currentHandHighest ? hand : currentHand)
+                    if(handHighest === currentHandHighest) tie = true;
+
+                    hand = (UTIL.values.indexOf(handHighest) > UTIL.values.indexOf(currentHandHighest) ? hand : currentHand)
                 }
             }
-
+            if(tie) console.log("tie")
             return hand;
-        }
+            // end of game logic for round 1
+
+        } else if(this.dealer.hand.length < 5){ //look for high card, pair, two pair, 3-of-a-kind, 4-of-a-kind
+            let handIdx = 0;
+            hand = this.players[0].hand.concat(this.dealer.hand)
+            // console.log(hand)
+
+            for(let i = 1; i < this.players.length; i++){
+                currentHand = this.players[i].hand.concat(this.dealer.hand)
+
+                //hashes to count pairs*
+                let handCount = {};
+                let currHandCount = {};
+                for(let i = 0; i < hand.length; i++){
+                    (handCount[hand[i].value]) ? handCount[hand[i].value].push(i) : handCount[hand[i].value] = [i];
+                    (currHandCount[currentHand[i].value]) ? currHandCount[currentHand[i].value].push(i) : currHandCount[currentHand[i].value] = [i];
+                }
+
+                if (Object.values(handCount).find((ele) => ele.length > 4) || Object.values(currHandCount).find((ele) => ele.length > 4)){
+                    handFourOfKind = Object.values(handCount).find((ele) => ele.length > 4);
+                    currHandFourOFKind = Object.values(currHandCount).find((ele) => ele.length > 4);
+                    if(handFourOfKind && currHandFourOFKind){
+                        if(UTIL.values.indexOf(handFourOfKind[0]) === UTIL.values.indexOf(currHandFourOFKind[0])){
+                            tie = true;
+                        }else if (UTIL.values.indexOf(handFourOfKind[0]) < UTIL.values.indexOf(currHandFourOFKind[0])){
+                            
+                        }
+                    }else{
+                        if (currHandFourOFKind) hand = currHandCount;
+                    }
+                }else if (Object.values(handCount).find((ele) => ele.length > 3) || Object.values(currHandCount).find((ele) => ele.length > 3)){
+
+                }else if (Object.values(handCount).find((ele) => ele.length > 22) || Object.values(currHandCount).find((ele) => ele.length > 2)){
+
+                }
+                // console.log(Object.values(handCount))
+                // console.log(handCount)
+                // console.log(currHandCount)
+                
+            }
+            return this.players[handIdx].hand;
+        }else{ //case: dealer has all 5 cards
+            let dealerHand = this.dealer.hand;
+            // console.log(dealerHand)
+            hand = this.players[0].hand.concat(dealerHand);
+            // console.log(this.players[0].hand);
+
+            for(let i = 1; i < this.players.length; i++){
+                currentHand = this.players[i].hand.concat(dealerHand);
+                // need logic to check what kind of hand they have;
+            }
+
+            if(tie) console.log(tie)
+            return hand;
+        } //end of all other rounds logic
     }
 
 }
 class PlayerHand{
     constructor(deck){
         this.deck = deck; // reference to the game's card deck
-        this.hand = []; // array that will holf this hand's cards
+        this.hand = []; // array that will hold this player's cards
     }
 
 
     addToHand(num){
-        //take a card from deck and add to hand
+        //take a card from deck and add to hand, remove card from deck
         while(num > 0){
             this.hand.push(this.deck.shift());
             num--;
@@ -107,7 +167,8 @@ let game = new multiChoice();
 game.hands.call(game, 5);
 console.log(game);
 game.printGame.call(game, 5);
-console.log(game.bestHand.call(game, 1));
+console.log("Winning card")
+console.log(game.bestHand.call(game, 3));
 
 ///build a database with possible questions to query for to use during each round
 
--- src/scripts/util.js
+++ src/scripts/util.js
@@ -2,4 +2,5 @@
     //variables for creation of cards/deck and scoring purposes
 export const suits = ['♣', '♦', '♥', '♠']
 export const values = ['A', 'K', 'Q', 'J', 10, 9, 8, 7, 6, 5, 4, 3, 2].reverse();
-export const letterValue = {'A': 14, 'K': 13, 'Q': 12, 'J': 11}
+// export const letterValue = {'A': 14, 'K': 13, 'Q': 12, 'J': 11}
+export const rankingHands = ['high card', 'pair', 'two pairs', 'three of a kind', 'striaght', 'flush', 'full house', 'four of a kind', 'striaght flush', 'royal flush'];
